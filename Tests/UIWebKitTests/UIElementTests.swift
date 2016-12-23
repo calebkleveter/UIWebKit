9074acde4551af0ef42282f8d1b65b6ada103021
Caleb <caleb.kleveter@gmail.com>
2016-12-23 12:28:14 -0600

    Finished UIElement test implimentation

--- Tests/UIWebKitTests/UIElementTests.swift
+++ Tests/UIWebKitTests/UIElementTests.swift
@@ -2,7 +2,14 @@ import XCTest
 @testable import UIWebKit
 
 class UIElementTests: XCTestCase {
-    static let allTests = []
+    static let allTests = [
+        ("Test Element Start", testElementStart),
+        ("Test Element End", testElementEnd),
+        ("Test Element Attributes", testAttributes),
+        ("Test Element Text", testText),
+        ("Test Element Children", testChildren),
+        ("Test Element Parse", testParse)
+    ]
     
     func testElementStart() {
         let element = UIElement(element: .p)
@@ -12,5 +19,35 @@ class UIElementTests: XCTestCase {
     func testElementEnd() {
         let element = UIElement(element: .a)
         XCTAssert(element.end == "</a>")
+    }
+    
+    func testAttributes() {
+        let element = UIElement(element: .div)
+        element.attributes["style"] = "color: orange;"
+        XCTAssert(element.attributes["style"] == "color: orange;")
+    }
+    
+    func testText() {
+        let element = UIElement(element: .p)
+        element.text = "Hello World"
+        XCTAssert(element.text == "Hello World")
+    }
+    
+    func testChildren() {
+        let element = UIElement(element: .div)
+        let child = UIElement(element: .p)
+        element.children.append(child)
+        XCTAssert(element.children[0] == child)
+    }
+    
+    func testParse() {
+        let element = UIElement(element: .div)
+        element.attributes["style"] = "color: orange;"
+        let child = UIElement(element: .p)
+        element.children.append(child)
+        
+        let html = element.parse()
+        
+        XCTAssert(html == "<div style='color: orange;' ><p></p></div>")
     }
 }
