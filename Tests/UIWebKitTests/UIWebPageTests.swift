52efb8ef4efdef86a69a9003316667e34d2aa6e2
Caleb <caleb.kleveter@gmail.com>
2016-12-23 14:47:13 -0600

    Added .testElements() to UIWebPageTests

--- Tests/UIWebKitTests/UIWebPageTests.swift
+++ Tests/UIWebKitTests/UIWebPageTests.swift
@@ -2,5 +2,20 @@ import XCTest
 @testable import UIWebKit
 
 class UIWebPageTests: XCTestCase {
-    static let allTests = []
+    static let allTests = [
+        ("Tests Page Elements", testElements)
+    ]
+    
+    func testElements() {
+        let head = UIElement(element: .head)
+        let header = UIElement(element: .header)
+        let section = UIElement(element: .section)
+        let footer = UIElement(element: .footer)
+        let page = UIWebPage(head: head, header: header, section: section, footer: footer)
+        
+        XCTAssert(page.head == head)
+        XCTAssert(page.header == header)
+        XCTAssert(page.section == section)
+        XCTAssert(page.footer == footer)
+    }
 }
