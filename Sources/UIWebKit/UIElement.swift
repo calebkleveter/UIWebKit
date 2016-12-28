975e58cee94e40fc915dbf67d39387b7332fe7f1
Caleb <caleb.kleveter@gmail.com>
2016-12-28 14:25:11 -0600

    Added empty tag support to UIElement init

--- Sources/UIWebKit/UIElement.swift
+++ Sources/UIWebKit/UIElement.swift
@@ -44,8 +44,82 @@ public class UIElement {
     ///
     /// - returns: An instance of UIElement with it's properties initilized with the element passed in.
     public init(element: Element) {
-        
         switch element {
+        case .area:
+            self.element = element
+            self.start = "<\(element.rawValue)/>"
+            self.end = ""
+            self.isSingleTag = true
+        case .base:
+            self.element = element
+            self.start = "<\(element.rawValue)/>"
+            self.end = ""
+            self.isSingleTag = true
+        case .br:
+            self.element = element
+            self.start = "<\(element.rawValue)/>"
+            self.end = ""
+            self.isSingleTag = true
+        case .col:
+            self.element = element
+            self.start = "<\(element.rawValue)/>"
+            self.end = ""
+            self.isSingleTag = true
+        case .colgroup:
+            self.element = element
+            self.start = "<\(element.rawValue)/>"
+            self.end = ""
+            self.isSingleTag = true
+        case .embed:
+            self.element = element
+            self.start = "<\(element.rawValue)/>"
+            self.end = ""
+            self.isSingleTag = true
+        case .hr:
+            self.element = element
+            self.start = "<\(element.rawValue)/>"
+            self.end = ""
+            self.isSingleTag = true
+        case .img:
+            self.element = element
+            self.start = "<\(element.rawValue)/>"
+            self.end = ""
+            self.isSingleTag = true
+        case .input:
+            self.element = element
+            self.start = "<\(element.rawValue)/>"
+            self.end = ""
+            self.isSingleTag = true
+        case .link:
+            self.element = element
+            self.start = "<\(element.rawValue)/>"
+            self.end = ""
+            self.isSingleTag = true
+        case .meta:
+            self.element = element
+            self.start = "<\(element.rawValue)/>"
+            self.end = ""
+            self.isSingleTag = true
+        case .param:
+            self.element = element
+            self.start = "<\(element.rawValue)/>"
+            self.end = ""
+            self.isSingleTag = true
+        case .source:
+            self.element = element
+            self.start = "<\(element.rawValue)/>"
+            self.end = ""
+            self.isSingleTag = true
+        case .track:
+            self.element = element
+            self.start = "<\(element.rawValue)/>"
+            self.end = ""
+            self.isSingleTag = true
+        case .wbr:
+            self.element = element
+            self.start = "<\(element.rawValue)/>"
+            self.end = ""
+            self.isSingleTag = true
         default:
             self.element = element
             self.start = "<\(element.rawValue)>"
