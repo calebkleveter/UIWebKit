7bcb28257ce5bc950babcf253eb1e49027fe235e
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-02-09 17:51:36 -0600

    Sources/UIWebKit/Dependancies.swift - Created Dependancy.htmlTags CP

--- Sources/UIWebKit/Dependancies.swift
+++ Sources/UIWebKit/Dependancies.swift
@@ -35,4 +35,11 @@ enum Dependancy {
         case .bootstrap: return css
         }
     }
+    
+    var htmlTags: [DependancyType: [String]] {
+        switch self {
+        case .bootstrap: return [.css: ["<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">", "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css\" integrity=\"sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp\" crossorigin=\"anonymous\">"], .javaScript: ["<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>"]]
+        case .jQuery: return [.javaScript: ["<script src=\"https://code.jquery.com/jquery-3.1.1.min.js\" integrity=\"sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=\"crossorigin=\"anonymous\"></script>"]]
+        }
+    }
 }
