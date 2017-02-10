2f970e2e7ea8bddafe43e86395da1d1b3b5d453a
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-02-10 11:19:40 -0600

    Sources/UIWebKit/Dependancies.swift - Documented all code

--- Sources/UIWebKit/Dependancies.swift
+++ Sources/UIWebKit/Dependancies.swift
@@ -20,15 +20,25 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
+/// Although dependencies use more CSS or JavaScript, this enum is used more for adding the CSS of the dependeny to the head of the page and the below the footer of the page.
+///
+/// - javaScript: Defines the CDN links of the dependency as linking to JavaScript.
+/// - css: Defines the CDN links of the dependency as linking to CSS.
 public enum DependancyType {
     case javaScript
     case css
 }
 
+/// A dependency that a UIWebPage loads in.
 public enum Dependancy {
+    
+    /// This case is for loading jQuery into a UIWebPage.
     case jQuery
+    
+    /// This case is used for loading Twitter Bootstrap into a UIWebPage.
     case bootstrap
     
+    /// Gets the type of the dependency. This is based off what the dependency uses _mostly_. For example, Twitter Bootstrap is marked as CSS, but it also uses JS.
     public var type: DependancyType {
         switch self {
         case .jQuery: return .javaScript
@@ -36,6 +46,7 @@ public enum Dependancy {
         }
     }
     
+    /// Returns a a dictionary with the key as the type of CDN links that are used in the value and the value as an array of Strings that are the CDN links to the dependency.
     public var htmlTags: [DependancyType: [String]] {
         switch self {
         case .bootstrap: return [.css: ["<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">", "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css\" integrity=\"sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp\" crossorigin=\"anonymous\">"], .javaScript: ["<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>"]]
