facf51c7a4d215d60d5358ddf696afc2ad734ef8
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-02-20 15:57:33 -0600

    Sources/UIWebKit/ElementPresentable.swift - Created ElementPresentable.topElement var

--- Sources/UIWebKit/ElementPresentable.swift
+++ Sources/UIWebKit/ElementPresentable.swift
@@ -20,4 +20,6 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-public protocol ElementPresentable {}
+public protocol ElementPresentable {
+    var topElement: UIElement { get }
+}
