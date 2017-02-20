c6fcc1e6acd168de3101f95e16e0e78334bf0129
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-02-20 14:12:39 -0600

    Sources/UIWebKit/Components/UIForm.swift - Created LoginFormType enum

--- Sources/UIWebKit/Components/UIForm.swift
+++ Sources/UIWebKit/Components/UIForm.swift
@@ -20,6 +20,10 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
+public enum LoginFormType {
+    
+}
+
 open class UIForm {
     public let form = UIElement(element: .form)
 }
