88083f5eda9d7d6ef57d02b5e9b7bbf7f0487904
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-02-20 14:10:21 -0600

    Sources/UIWebKit/Components/UIForm.swift - Created UIForm.form const

--- Sources/UIWebKit/Components/UIForm.swift
+++ Sources/UIWebKit/Components/UIForm.swift
@@ -20,4 +20,6 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-open class UIForm {}
+open class UIForm {
+    public let form = UIElement(element: .form)
+}
