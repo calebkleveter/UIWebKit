4dd35c75b4e91098bd3e38e3ebd252523740e7e9
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-02-21 07:57:26 -0600

    Sources/UIWebKit/ElementRenderable.swift - Created ElementRenderable protocol

--- Sources/UIWebKit/ElementRenderable.swift
+++ Sources/UIWebKit/ElementRenderable.swift
@@ -19,3 +19,6 @@
 //  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
+
+/// Designates an object as capable of being rendered into HTML.
+public protocol ElementRenderable {}
