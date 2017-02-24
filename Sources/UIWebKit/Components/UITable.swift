b1b834fc3dd59ad0670bd470ff7096176298dd6e
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-02-24 17:33:03 -0600

    Sources/UIWebKit/Coponents/UITable.swift - Error - Created UITable.table const

--- Sources/UIWebKit/Components/UITable.swift
+++ Sources/UIWebKit/Components/UITable.swift
@@ -20,7 +20,9 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-open class UITable {}
+open class UITable {
+    let table: UIElement
+}
 
 /// A row in an HTML table.
 open class UITableRow {
