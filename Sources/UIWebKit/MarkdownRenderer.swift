dfe35af04090bbd1876706906fc4b87ceb75c8a6
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-02-28 12:01:07 -0600

    Sources/UIWebKit/MarkdownRenderer.swift - Imported Foundation

--- Sources/UIWebKit/MarkdownRenderer.swift
+++ Sources/UIWebKit/MarkdownRenderer.swift
@@ -20,6 +20,8 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
+import Foundation
+
 /// Renders Markdown into HTML.
 public struct MarkdownRenderer {
     
