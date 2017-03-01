09393efbe0b248b619942f4fccbc028f543667c2
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-01 12:46:45 -0600

    Sources/UIWebKit/MarkdownRenderer.swift - Created MarkdownRenderer.replace(regex string start end)

--- Sources/UIWebKit/MarkdownRenderer.swift
+++ Sources/UIWebKit/MarkdownRenderer.swift
@@ -23,6 +23,11 @@
 import Foundation
 
 /// Renders Markdown into HTML.
-public struct MarkdownRenderer {
-    
+public struct rers {
+    public func replace(matchesFor regex: String, in string: String, start: String = "", end: String = "")throws -> String {
+        let mutableString = NSMutableString(string: string)
+        let regExpression = try NSRegularExpression(pattern: regex, options: .allowCommentsAndWhitespace)
+        regExpression.replaceMatches(in: mutableString, options: .withoutAnchoringBounds, range: NSRange(location: 0, length: mutableString.length), withTemplate: start + "$1" + end)
+        return String(mutableString)
+    }
 }
