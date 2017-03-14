92db08a59a66f7576c2de65c7912ade8515b25cb
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-14 13:54:16 -0500

    Package.swift - Added SwiftDown package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ let package = Package(
     // Name of Package here:
     name: "UIWebKit",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
+        .Package(url: "https://github.com/calebkleveter/SwiftDown.git", majorVersion: 0, minor: 1)
     ],
     exclude: [
         "Config",
