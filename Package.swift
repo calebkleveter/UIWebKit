7439552c36013a656a23e6fcaaccfc30f00b4cbb
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-24 09:42:41 -0500

    Package.swift - Updated SwiftDown version to 0.2.x

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "UIWebKit",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
-        .Package(url: "https://github.com/calebkleveter/SwiftDown.git", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/calebkleveter/SwiftDown.git", majorVersion: 0, minor: 2)
     ],
     exclude: [
         "Config",
