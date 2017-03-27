7252d331f18df4bc71620c27ce34d1e98f8326e7
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-27 11:29:39 -0500

    Sources/UIWebKit/Components/UIParagraph.swift - Created UIParagraph class

--- Sources/UIWebKit/Components/UIParagraph.swift
+++ Sources/UIWebKit/Components/UIParagraph.swift
@@ -19,3 +19,6 @@
 //  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
+
+/// `UIParagraph` is a wrapper class around a `p` HTML tag which is represented by a `UIElement`.
+open class UIParagraph {}
