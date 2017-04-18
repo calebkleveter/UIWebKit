98fbf1ff9fde48d78f52ea7c10ab49bc6b442d14
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-04-18 10:32:52 -0500

    Sources/UIWebKit/UIElement.swift - Changed usage of isSingleTag var to Element.isSingleTag

--- Sources/UIWebKit/UIElement.swift
+++ Sources/UIWebKit/UIElement.swift
@@ -25,169 +25,94 @@ import SwiftMark
 
 /// A UIElement works as the wrapper for an HTML element.
 public class UIElement {
-    
-    /// Denotes whether the element is a single tag element or not.
-    private var isSingleTag: Bool
-    
+
     /// The element that is returned when the UIElement is parsed.
     public var element: Element
-    
+
     /// The opening tag of the element.
     public var start: String
-    
+
     /// The closing tag of the element.
     public var end: String
-    
+
     /// Create an instance of UIElement that is the element passed in.
     ///
     /// - parameter element: The element that is created by `.parse()`.
     ///
     /// - returns: An instance of UIElement with it's properties initilized with the element passed in.
     public init(element: Element) {
-        switch element {
-        case .area:
+        switch element.isSingleTag {
+        case true:
             self.element = element
             self.start = "<\(element.rawValue)/>"
             self.end = ""
-            self.isSingleTag = true
-        case .base:
-            self.element = element
-            self.start = "<\(element.rawValue)/>"
-            self.end = ""
-            self.isSingleTag = true
-        case .br:
-            self.element = element
-            self.start = "<\(element.rawValue)/>"
-            self.end = ""
-            self.isSingleTag = true
-        case .col:
-            self.element = element
-            self.start = "<\(element.rawValue)/>"
-            self.end = ""
-            self.isSingleTag = true
-        case .colgroup:
-            self.element = element
-            self.start = "<\(element.rawValue)/>"
-            self.end = ""
-            self.isSingleTag = true
-        case .embed:
-            self.element = element
-            self.start = "<\(element.rawValue)/>"
-            self.end = ""
-            self.isSingleTag = true
-        case .hr:
-            self.element = element
-            self.start = "<\(element.rawValue)/>"
-            self.end = ""
-            self.isSingleTag = true
-        case .img:
-            self.element = element
-            self.start = "<\(element.rawValue)/>"
-            self.end = ""
-            self.isSingleTag = true
-        case .input:
-            self.element = element
-            self.start = "<\(element.rawValue)/>"
-            self.end = ""
-            self.isSingleTag = true
-        case .link:
-            self.element = element
-            self.start = "<\(element.rawValue)/>"
-            self.end = ""
-            self.isSingleTag = true
-        case .meta:
-            self.element = element
-            self.start = "<\(element.rawValue)/>"
-            self.end = ""
-            self.isSingleTag = true
-        case .param:
-            self.element = element
-            self.start = "<\(element.rawValue)/>"
-            self.end = ""
-            self.isSingleTag = true
-        case .source:
-            self.element = element
-            self.start = "<\(element.rawValue)/>"
-            self.end = ""
-            self.isSingleTag = true
-        case .track:
-            self.element = element
-            self.start = "<\(element.rawValue)/>"
-            self.end = ""
-            self.isSingleTag = true
-        case .wbr:
-            self.element = element
-            self.start = "<\(element.rawValue)/>"
-            self.end = ""
-            self.isSingleTag = true
-        default:
+        case false:
             self.element = element
             self.start = "<\(element.rawValue)>"
             self.end = "</\(element.rawValue)>"
-            self.isSingleTag = false
         }
     }
-    
+
     /// The attributes that are added to the element on parsing.
     public var attributes: [String: String] = [:]
-    
+
     /// The elements text.
     public private(set)var text: String = ""
-    
+
     /// The elements children.
     public private(set)var children: [ElementRenderable] = []
-    
+
     /// Child elements that are added in String format. Note that these elements are added as children _first_.
     public private(set)var rawElements: [String] = []
-    
+
     /// Adds text to an element _if_ it is not an empty element. The text is safety encoded.
     ///
     /// - parameter text: The text to add to the element.
     public func add(_ text: String) {
-        if !isSingleTag {
+        if !element.isSingleTag {
             self.text = text.safetyHTMLEncoded()
         }
     }
-    
+
     /// Adds text to an element _if_ it is not a single tag element. Note that this text is not safety encoded.
     ///
     /// - Parameter text: The text that will be added to the element. If you want to know more, ask Jon Skeet 🦄.
     public func addRaw(_ text: String) {
-        if !isSingleTag {
+        if !element.isSingleTag {
             self.text = text
         }
     }
-    
+
     /// Adds a child element to an element _if_ it is not an empty element.
     ///
     /// - parameter child: The element to add to the element.
     public func add(_ child: ElementRenderable) {
-        if !isSingleTag {
+        if !element.isSingleTag {
             self.children.append(child)
         }
     }
-    
+
     /// Adds a String to the rawElements array _if_ it is not a single tag element.
     ///
     /// - Parameter element: The element that will be added as a child of the current element.
     public func inject(_ element: String) {
-        if !isSingleTag {
+        if !self.element.isSingleTag {
             self.rawElements.append(element)
         }
     }
-    
-    
+
+
     /// Renders Markdown to HTML that gets added as child elements. This method uses `.inject(string)` for adding the elements.
     ///
     /// - Parameter string: The Markdown that will be rendered to HTML.
     public func addMarkdown(_ string: String)throws {
-        if !isSingleTag {
+        if !element.isSingleTag {
             let renderer = MarkdownRenderer()
             let html = try renderer.render(string)
             self.inject(html)
         }
     }
-    
+
     /// Creates HTML from the current element and all it's children. This method is deprecated due to bad naming. Use the `render()` method instead.
     ///
     /// - returns: The HTML from the current elements and it's children.
@@ -211,7 +136,7 @@ public class UIElement {
         html.append(self.end)
         return html
     }
-    
+
     /// Creates HTML from the current element and all it's children.
     ///
     /// - Returns: The HTML from the current elements and it's children.
@@ -233,7 +158,7 @@ public class UIElement {
         html.append(self.end)
         return html
     }
-    
+
     /// Adds the elements attributes to it's opening tag.
     private func appendAttributes() {
         var attr = ""
@@ -245,7 +170,7 @@ public class UIElement {
 }
 
 extension UIElement: ElementRenderable {
-    
+
     /// The top level element of the current element. This means `self`.
     public var topLevelElement: UIElement {
         return self
